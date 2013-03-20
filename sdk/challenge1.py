536e183ed6c097f81c17c15be8f5b245702d4e00
Matt Martz <matt@sivel.net>
2013-03-19 19:57:54 -0500

    Allow specification of number of servers in challenge 1

--- sdk/challenge1.py
+++ sdk/challenge1.py
@@ -10,14 +10,15 @@ import os
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('base', help='The base hostname to use, 3x512MB'
-                        ' servers will be built using this base hostname')
+    parser.add_argument('base', help='The base hostname to use, 3x512MB')
     parser.add_argument('--dc', required=False, help='The region to '
                         'build the servers in', choices=['DFW', 'ORD', 'LON'],
                         default=pyrax.default_region)
     parser.add_argument('--image', required=False, help='The image ID to build'
                         ' the servers with',
                         default='5cebb13a-f783-4f8c-8058-c4182c724ccd')
+    parser.add_argument('--count', required=False, help='Number of servers to '
+                        'build. Default 3', default=3, type=int)
 
     args = parser.parse_args()
 
@@ -26,7 +27,7 @@ def main():
     cs = pyrax.cloudservers
     print 'Building servers in: %s' % args.dc
     servers = {}
-    for i in xrange(0, 3):
+    for i in xrange(0, abs(args.count)):
         host = '%s%d' % (args.base, i)
         print 'Creating server: %s' % host
         servers[host] = cs.servers.create(host, args.image, 2)
